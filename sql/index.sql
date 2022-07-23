69dcd4d05ff488b909c574fd8d0e9374f366fc1f
daylight55 <jojomasanori3@gmail.com>
2022-07-23 14:03:09 +0900

    Fix

--- sql/index.sql
+++ sql/index.sql
@@ -1,3 +1,2 @@
 -- admin
-IF NOT EXISTS 
-ALTER TABLE visit_history ADD INDEX tenant_id_and_competition_id_idx(tenant_id, competition_id);
+-- ALTER TABLE visit_history ADD INDEX tenant_id_and_competition_id_idx(tenant_id, competition_id);
