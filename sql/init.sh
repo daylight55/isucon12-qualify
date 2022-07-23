8938711130536d2a8eec433a9fe9f52881ba7011
yoto1980yen <yotolemontea0725@yahoo.co.jp>
2022-07-23 12:43:46 +0900

    fix

--- sql/init.sh
+++ sql/init.sh
@@ -9,16 +9,6 @@ ISUCON_DB_USER=${ISUCON_DB_USER:-isucon}
 ISUCON_DB_PASSWORD=${ISUCON_DB_PASSWORD:-isucon}
 ISUCON_DB_NAME=${ISUCON_DB_NAME:-isuports}
 
-CURRENT_DIR=$(cd $(dirname $0);pwd)
-cd ${CURRENT_DIR}
-
-cat ./admin/10_schema.sql tenant/10_schema.sql | \
-mysql -u"$ISUCON_DB_USER" \
-		-p"$ISUCON_DB_PASSWORD" \
-		--host "$ISUCON_DB_HOST" \
-		--port "$ISUCON_DB_PORT" \
-		"$ISUCON_DB_NAME"
-
 # MySQLを初期化
 mysql -u"$ISUCON_DB_USER" \
 		-p"$ISUCON_DB_PASSWORD" \
