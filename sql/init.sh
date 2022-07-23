ff599fee1a970119c0bd2ab33d69cdc913e428f6
daylight55 <jojomasanori3@gmail.com>
2022-07-23 12:24:26 +0900

    Fix sql/init.sh

--- sql/init.sh
+++ sql/init.sh
@@ -9,6 +9,13 @@ ISUCON_DB_USER=${ISUCON_DB_USER:-isucon}
 ISUCON_DB_PASSWORD=${ISUCON_DB_PASSWORD:-isucon}
 ISUCON_DB_NAME=${ISUCON_DB_NAME:-isuports}
 
+cat ./admin/01_create_mysql_database.sql ./admin/10_Schema.sql tenant/10_Schema.sql | \
+mysql -u"$ISUCON_DB_USER" \
+		-p"$ISUCON_DB_PASSWORD" \
+		--host "$ISUCON_DB_HOST" \
+		--port "$ISUCON_DB_PORT" \
+		"$ISUCON_DB_NAME"
+
 # MySQLを初期化
 mysql -u"$ISUCON_DB_USER" \
 		-p"$ISUCON_DB_PASSWORD" \
